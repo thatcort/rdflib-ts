42342a4a340092e465cfbfe358b9522144f38efd
vladimir_djurdjevic <vladimirdjurdjevic93@gmail.com>
2017-05-06 22:28:59 +0200

    done

--- test/unit/utils/io/rdf-data-importer.unit.spec.ts
+++ test/unit/utils/io/rdf-data-importer.unit.spec.ts
@@ -12,12 +12,12 @@ describe('RdfDataImporter - Unit', () => {
 	context('constructor', () => {
 		it('should set default options for non specified ones', () => {
 			let exporter = new RdfDataImporter();
-			exporter.options.skolemize.should.be.true;
+			exporter.options.skolemize.should.be.false;
 			exporter.options.importChunkSize.should.equal(1000);
 			exporter.options.should.not.haveOwnProperty('defaultGraph');
 
 			exporter = new RdfDataImporter({});
-			exporter.options.skolemize.should.be.true;
+			exporter.options.skolemize.should.be.false;
 			exporter.options.importChunkSize.should.equal(1000);
 			exporter.options.should.not.haveOwnProperty('defaultGraph');
 
